f3a6f820ab3573a67602288100e0d84f1b8076fb
Ayaan <onestar7984@gmail.com>
2023-12-21 15:33:02 +0900

    refactor: 일정 시간마다 선택된 image를 자동으로 넘기는 기능 추가

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -13,6 +14,8 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   final ImagePicker _picker = ImagePicker();
   List<XFile>? images;
+  int _currentPage = 0;
+  final PageController _pageController = PageController();
 
   @override
   void initState() {
@@ -22,6 +25,13 @@ class _MainScreenState extends State<MainScreen> {
 
   Future<void> loadImages() async {
     images = await _picker.pickMultiImage();
+
+    if (images != null) {
+      Timer.periodic(const Duration(seconds: 5), (timer) {
+        _currentPage = _currentPage == images!.length - 1 ? 0 : _currentPage + 1;
+        _pageController.animateToPage(_currentPage, duration: const Duration(milliseconds: 350), curve: Curves.easeIn);
+      });
+    }
 
     setState(() {});
   }
@@ -36,19 +46,24 @@ class _MainScreenState extends State<MainScreen> {
           ? const Center(
               child: Text('No data'),
             )
-          : FutureBuilder<Uint8List>(
-              future: images![0].readAsBytes(),
-              builder: (context, snapshot) {
-                final data = snapshot.data;
-                if (data == null || snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                return Image.memory(
-                  data,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+          : PageView(
+              controller: _pageController,
+              children: images!.map((image) {
+                return FutureBuilder<Uint8List>(
+                  future: image.readAsBytes(),
+                  builder: (context, snapshot) {
+                    final data = snapshot.data;
+                    if (data == null || snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    return Image.memory(
+                      data,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    );
+                  },
                 );
-              },
+              }).toList(),
             ),
     );
   }
