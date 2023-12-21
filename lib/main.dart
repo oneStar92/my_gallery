0c6cc7e8bc9b99f8cb61d8b8a43b464a10b19721
Ayaan <onestar7984@gmail.com>
2023-12-21 14:56:37 +0900

    feat: MainScreen Type 정의

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_gallery/main_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: MainScreen(),
     );
   }
 }
